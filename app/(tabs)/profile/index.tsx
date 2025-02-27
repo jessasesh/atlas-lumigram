e6fb451ebd92279765e34dc05e82372563d69bdf
jessasesh <jessica.dison@yahoo.com>
2025-02-26 21:53:41 -0600

    Placeholder for logged in users profile

---
+++ app/(tabs)/profile/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function MyProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>Welcome, Jess!</Text>
+    </View>
+  );
+}
