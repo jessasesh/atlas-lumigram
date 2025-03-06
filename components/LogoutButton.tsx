b41afe6d8846ee0d1f68fc1e4b55f5471308f02f
jessasesh <jessica.disom@yahoo.com>
2025-03-06 00:00:34 -0600

    Better handling for logout

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,15 +1,19 @@
 import { useRouter } from "expo-router";
 import { Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useAuth } from "@/AuthProvider";
 
 export default function LogoutButton() {
+  const { logout } = useAuth();
   const router = useRouter();
 
+  const handleLogout = async () => {
+    await logout();
+    router.replace("/login");
+  };
+
   return (
-    <Pressable
-      onPress={() => router.replace("/login")}
-      style={{ padding: 10, marginRight: 15 }}
-    >
+    <Pressable onPress={handleLogout} style={{ padding: 10, marginRight: 15 }}>
       <Ionicons name="log-out-outline" size={24} color="#00D4B1" />
     </Pressable>
   );
